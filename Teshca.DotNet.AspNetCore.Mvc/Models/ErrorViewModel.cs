c386e45b6cd5ba717303fa17d7d11f53b98d8742
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2021-04-07 20:52:16 +1000

    new asp.net core mvc 5.0 project from template

---
+++ Teshca.DotNet.AspNetCore.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Teshca.DotNet.AspNetCore.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
